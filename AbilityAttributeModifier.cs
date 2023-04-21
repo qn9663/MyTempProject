3e10c075ca118f765ba36b10370c0bf9170681ea
Qing Nan Weng <mr_qn@hotmail.com>
2023-04-22 07:51:43 +0800

    Add files via upload

---
+++ AbilityAttributeModifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class AbilityAttributeModifier
+    {
+
+        public string Uuid;
+        public string AbilityConfigId;
+        public BaseAttributeType AttributeType;
+        public double Value;
+        public bool IsValueType;
+        public int StackingCount;
+        public string CreaateFromAbilityUuid;
+
+        public AbilityAttributeModifier()
+        {
+            this.Uuid = System.Guid.NewGuid().ToString("N");
+        }
+
+        public AbilityAttributeModifier(AbilityAttributeModifier from)
+        {
+            this.Uuid = System.Guid.NewGuid().ToString("N");
+            this.AttributeType = from.AttributeType;
+            this.Value = from.Value;
+            this.IsValueType = from.IsValueType;
+            this.StackingCount = from.StackingCount;
+        }
+    }
+}
