5269f928b2a27af55c4b064cddff5332892436fe
Qing Nan Weng <mr_qn@hotmail.com>
2023-04-22 13:17:49 +0800

    Add files via upload

---
+++ AbilityEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GameLogic
+{
+
+    public partial class Events
+    {
+
+        public delegate void OnAbilityTrigger(UnitInstance unit, AbilityInstance ability,bool isTrigger);
+
+    }
+
+
+    public partial interface IEventBus
+    {
+
+        Events.OnAbilityTrigger OnAbilityTrigger { get; set; }
+
+    }
+
+
+    public partial class EventBus //: MonoBehaviour, GameLogic.IEventBus
+    {
+        public Events.OnAbilityTrigger OnAbilityTrigger { get; set; }
+
+    }
+}
